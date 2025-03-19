8a1ae504bf625ca809bc04f209637e278d68a844
bankmotion <harris5555555555555@gmail.com>
2025-03-19 14:32:01 -0500

    Fix Get Started button postion on Home

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -93,7 +93,7 @@ const useStyles = makeStyles()((theme) => ({
   },
 
   startButton: {
-    marginTop: theme.spacing(5),
+    marginTop: theme.spacing(8),
     padding: theme.spacing(0.5, 2),
     fontSize: "1.2rem",
     fontWeight: 500,
