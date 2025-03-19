858cf46627d55428bff165bbcbd36daf21915937
bankmotion <harris5555555555555@gmail.com>
2025-03-19 17:25:27 -0500

    Fix letter

--- src/components/Auth/Signup/Signup.tsx
+++ src/components/Auth/Signup/Signup.tsx
@@ -16,7 +16,7 @@ import useStyles from "./styles";
 const planOptions = [
   { label: "Writer - $15/month", value: "writer" },
   { label: "Agency&Team - $75: 1 user, 5 matches", value: "agency75" },
-  { label: "Agency&Team - $150: 3 user, 15 matches", value: "agency150" },
+  { label: "Agency&Team - $150: 3 users, 15 matches", value: "agency150" },
   { label: "Agency&Team - $250: unlimited, priority", value: "agency250" },
 ];
 
